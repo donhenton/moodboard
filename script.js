c1036c7e0c66380d2fc14c74efcf3825433210db
Don Henton <donhenton@gmail.com>
2026-08-25 10:15:48 -0500

    Add new image paths to script.js
    Added new image paths to the array in script.js.

--- script.js
+++ script.js
@@ -10,6 +10,17 @@ const images = [
     // "images/photo02.jpg",
     // "images/photo03.png",
     // "images/photo04.webp"
+
+'images/2fa7c4c2b47a3234982fc55f925ad787.jpg',
+'images/584a5e97d8760365916e1a508f1b255a.jpg',
+'images/5bc9c5dd3cc8dc65b2ee305f84757433.jpg',
+'images/745c777a0ca86060f0f87c6ca5bb52a6.jpg',
+'images/74c255f0c64097ff45d558de2a06f9d7.jpg',
+'images/7a44b218112c86f2f6deb80d496c84ef.jpg',
+'images/b71dc8343e5ad05030d641d411d23b84.jpg'
+
+
+    
 ];
 
 const board = document.getElementById("board");
