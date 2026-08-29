fd5127a98a2fd24c936f0f4cc4b60a54262063a9
Don Henton <donhenton@gmail.com>
2026-08-29 14:19:39 -0500

    second topic

--- topic2.js
+++ topic2.js
@@ -1,6 +1,26 @@
 const images = [
  
 
+'images2/a001.png',
+'images2/p001.jpg',
+'images2/p002.jpg',
+'images2/p003.jpg',
+'images2/p004.jpg',
+'images2/p005.jpg',
+'images2/p006.jpg',
+'images2/p007.jpg',
+'images2/p008.jpg',
+'images2/p009.jpg',
+'images2/p010.jpg',
+'images2/p011.jpg',
+'images2/p012.jpg',
+'images2/p013.jpg',
+
+'images2/b001.jpg',
+'images2/b002.jpg',
+'images2/b003.jpg',
+'images2/b004.jpg',
+'images2/b005.jpg' 
 
 
     
