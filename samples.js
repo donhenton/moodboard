08faf96706c5ff24bf404c9fd2dc0742ee6a0e80
Don Henton <donhenton@gmail.com>
2026-08-30 11:56:56 -0500

    rewrite

---
+++ samples.js
@@ -0,0 +1,16 @@
+const images = [
+ 
+
+'samples/p0007.png' ,  
+'samples/IMG_1729.jpg',
+'samples/IMG_1751.jpg',
+'samples/IMG_1853.jpg',
+'samples/IMG_1872.jpg',
+'samples/IMG_1660.jpg',
+'samples/IMG_1529.jpg',
+'samples/lighting_layout.PNG',
+'samples/tall faced dude 1.PNG'
+
+
+    
+];
