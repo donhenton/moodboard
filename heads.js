a519946a09ccfc6d5de239dc9a187442f8cadca8
Don Henton <donhenton@gmail.com>
2026-08-31 17:15:49 -0500

    first stage complete

--- heads.js
+++ heads.js
@@ -5,7 +5,16 @@ const images = [
  
 'heads/h003.jpg',
 'heads/h004.jpg',
+'heads/h005.png',
 'heads/h006.png',
+'heads/IMG_1853.jpg',
+
+'heads/cam2.png',
+'heads/cam3.png',
+'heads/cam4.png',
+
+
+
 'heads/p0007.png' ,  
 'heads/IMG_1751.jpg',
 'heads/tall faced dude 1.PNG',
--- stages.js
+++ stages.js
@@ -1,21 +1,23 @@
 const images = [
  
 
-'stages/2fa7c4c2b47a3234982fc55f925ad787.jpg',
-'stages/584a5e97d8760365916e1a508f1b255a.jpg',
+
+
 'stages/5bc9c5dd3cc8dc65b2ee305f84757433.jpg',
-//'stages/745c777a0ca86060f0f87c6ca5bb52a6.jpg', 
+
  'stages/74c255f0c64097ff45d558de2a06f9d7.jpg',
 'stages/7a44b218112c86f2f6deb80d496c84ef.jpg',
 'stages/b71dc8343e5ad05030d641d411d23b84.jpg',
 'stages/p0001.webp',
 'stages/p0002.webp',
+'stages/king_queen.webp',
 'stages/p0003.webp',
 'stages/p0004.webp',
-//'stages/p0005.png',
+'stages/p0007.png' ,
+'stages/cam1.png' ,
 'stages/p0006.jpg',
 'stages/p0007.jpg',
-'stages/p0007.png' ,
+'stages/scr1.png' ,
 'stages/IMG_1729.jpg',
 'stages/IMG_1751.jpg',
 'stages/IMG_1853.jpg',
