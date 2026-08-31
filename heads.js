8e09c80b972eb19c51294903e0366ff3c884867a
Don Henton <donhenton@gmail.com>
2026-08-31 16:28:31 -0500

    samples done

--- heads.js
+++ heads.js
@@ -2,13 +2,13 @@ const images = [
  
 
 'heads/h001.png',
-'heads/h002.png',
+ 
 'heads/h003.jpg',
 'heads/h004.jpg',
-'heads/h005.png',
+'heads/h006.png',
 'heads/p0007.png' ,  
 'heads/IMG_1751.jpg',
 'heads/tall faced dude 1.PNG',
-
-    
+'heads/p7finished.png' , 
+    'heads/p7_src.jpg' , 
 ];
--- samples.js
+++ samples.js
@@ -8,9 +8,14 @@ const images = [
 'samples/IMG_1872.jpg',
 'samples/IMG_1660.jpg',
 'samples/IMG_1529.jpg',
-'samples/lighting_layout.PNG' 
-
-
+'samples/lighting_layout.PNG' ,
+'samples/IMG_1167.JPG',
+'samples/IMG_1340.JPG',
+'samples/IMG_1359.jpg',
+'samples/IMG_1360.jpg',
+'samples/IMG_1361.jpg',
+'samples/IMG_1419.jpg',
+'samples/IMG_1605.jpg' 
 
     
 ];
