644a6bc36a2fc05b796264eb868c6c5b2267dea6
Don Henton <donhenton@gmail.com>
2026-08-31 19:11:55 -0500

    Comment out unused image paths
    Comment out unused image file paths in stages.js

--- stages.js
+++ stages.js
@@ -5,7 +5,7 @@ const images = [
 
 'stages/5bc9c5dd3cc8dc65b2ee305f84757433.jpg',
 
- 'stages/74c255f0c64097ff45d558de2a06f9d7.jpg',
+// 'stages/74c255f0c64097ff45d558de2a06f9d7.jpg',
 'stages/7a44b218112c86f2f6deb80d496c84ef.jpg',
 'stages/b71dc8343e5ad05030d641d411d23b84.jpg',
 'stages/p0001.webp',
@@ -18,8 +18,8 @@ const images = [
 'stages/p0006.jpg',
 'stages/p0007.jpg',
 'stages/scr1.png' ,
-'stages/IMG_1729.jpg',
-'stages/IMG_1751.jpg',
+//'stages/IMG_1729.jpg',
+//'stages/IMG_1751.jpg',
 'stages/IMG_1853.jpg',
 'stages/IMG_1872.jpg',
 'stages/lighting_layout.PNG',
