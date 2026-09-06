b39add88c6ef77c388532370216f11ef54a7a72b
Don Henton <donhenton@gmail.com>
2026-09-06 15:24:33 -0500

    added IMG_1909

--- taichi.js
+++ taichi.js
@@ -6,6 +6,7 @@ const images = [
 'taichi/584a5e97d8760365916e1a508f1b255a.jpg',
 'taichi/745c777a0ca86060f0f87c6ca5bb52a6.jpg',
 'taichi/74c255f0c64097ff45d558de2a06f9d7.jpg',
+'taichi/IMG_1909.jpg',
 'taichi/p0005.png',
 'taichi/parry_and_punch.png',
 'taichi/separate_arms_and_kick_render.png',
