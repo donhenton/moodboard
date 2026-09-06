64e9120237eb6ff486c3200816cc478993342262
Don Henton <donhenton@gmail.com>
2026-09-06 08:15:06 -0500

    tai chi section

---
+++ taichi.js
@@ -0,0 +1,15 @@
+const images = [
+ 
+
+
+'taichi/2fa7c4c2b47a3234982fc55f925ad787.jpg',
+'taichi/584a5e97d8760365916e1a508f1b255a.jpg',
+'taichi/745c777a0ca86060f0f87c6ca5bb52a6.jpg',
+'taichi/74c255f0c64097ff45d558de2a06f9d7.jpg',
+'taichi/p0005.png',
+'taichi/parry_and_punch.png',
+'taichi/separate_arms_and_kick_render.png',
+'taichi/snake_down.png' 
+
+    
+];
