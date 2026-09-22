33497b2438c99eef9a7f862cbbd43548c8c9dfbb
Don Henton <donhenton@gmail.com>
2026-09-22 15:55:52 -0500

    adding 70s covers

---
+++ 70_covers.js
@@ -0,0 +1,22 @@
+const images = [
+ 
+
+
+
+
+'70_covers/cv1.jpg',
+'70_covers/cv10.jpg',
+'70_covers/cv2.jpg',
+'70_covers/cv3.jpg',
+'70_covers/cv4.jpg',
+'70_covers/cv5.jpg',
+'70_covers/cv6.jpg',
+'70_covers/cv7.jpg',
+'70_covers/cv8.jpg',
+'70_covers/cv9.jpg' 
+
+
+
+
+    
+];
